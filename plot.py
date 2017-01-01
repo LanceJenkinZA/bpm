aa06633ba7cd161110a4f35ebbfd5165489cd760
chz <chz@bingularity.org>
2016-12-31 21:43:03 -0800

    plot: be clearer about timing

--- plot.py
+++ plot.py
@@ -3,13 +3,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 
-buf = [0]*300
-plt.axis([0, len(buf), 0, 16])
+# 60 Hz, 5s window
+buf = [0]*60*5
+plt.axis([0, len(buf), 0, 15])
 plt.ion()
 
 while True:
     plt.cla()
-    for i in range(16):
+    # update 6 times a second
+    for i in range(10):
         buf = buf[1:] + [int(sys.stdin.readline())]
     plt.plot(range(len(buf)), buf)
     plt.pause(0.0001)
